36cea4c825ab2adcbb08dcdd7cc939a43c6650c7
Mahaashree <mahaashreeofficial@gmail.com>
2025-06-13 23:07:44 +0530

    Finale touches

--- src/components/homepage/works.jsx
+++ src/components/homepage/works.jsx
@@ -6,30 +6,7 @@ import Card from "../common/card";
 import "./styles/works.css";
 
 const Works = () => {
-	return (
-		<div className="works">
-			<Card
-				icon={faBriefcase}
-				title="Work"
-				body={
-					<div className="works-body">
-						<div className="work">
-							<img
-								src="./facebook.png"
-								alt="facebook"
-								className="work-image"
-							/>
-							<div className="work-title">Facebook</div>
-							<div className="work-subtitle">
-								Software Engineer
-							</div>
-							<div className="work-duration">2019 - Present</div>
-						</div>
-					</div>
-				}
-			/>
-		</div>
-	);
+	return null;
 };
 
 export default Works;
